ebf5463cb53b79d795bc84af7f45a2b8a7c607f7
magnuskiro <magnuskiro@gmail.com>
2011-05-12 01:07:43 +0200

    smal css change

--- core/footer.php
+++ core/footer.php
@@ -1,4 +1,3 @@
 ﻿<div id="footer">
-	<p class="legal">Copyright (c) 2010 <a href=mailto:magnuskiro@gmail.com>Magnus Kirø</a> - Frivillig på Slottsfjell. Rights reserved.</p>
-	<p class="credit">Initial designed by <a href="http://www.nodethirtythree.com/">NodeThirtyThree</a> + <a href="http://www.freecsstemplates.org/">Free CSS Templates</a></p>
+	<p class="legal">Copyright (c) 2010-2011 <a href=mailto:magnuskiro@gmail.com>Magnus Kirø</a> - Frivillig på Slottsfjell. Rights reserved.</p>
 </div>
